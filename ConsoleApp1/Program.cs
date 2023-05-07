a291ce25213543644e58e86ea70c513606775109
matthew <mathewsun@rambler.ru>
2023-05-07 16:10:55 +0300

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine().Trim();
+            string str2 = Console.ReadLine().Trim();
+
+            str1 = str1.Remove(0, 1);
+            str1 = str1.Remove(str1.Length - 1);
+            str1 = str1.Replace(",", string.Empty);
+            int str1Int = Int32.Parse(str1);
+
+            str2 = str2.Remove(0, 1);
+            str2 = str2.Remove(str2.Length - 1);
+            str2 = str2.Replace(",", string.Empty);
+            int str2Int = Int32.Parse(str2);
+
+            int result = str1Int + str2Int;
+
+            string resultStr = result.ToString();
+
+            string output = "[";
+
+            foreach (char item in resultStr)
+            {
+                output = output + item + ",";
+            }
+
+            output = output.Remove(output.Length - 1);
+
+            output = output + "]";
+
+
+            Console.WriteLine(output); ;
+        }
+    }
+}
